85d5a91342949bf8c1233e3462fb3e807b2447a4
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-05-10 21:04:48 +0430

    added navigation_main.xml and MainActivity.kt navigation setup

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/MainActivity.kt
@@ -2,10 +2,35 @@ package com.jmzd.ghazal.recipeappmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.jmzd.ghazal.recipeappmvvm.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
 
 class MainActivity : AppCompatActivity() {
+    //Binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    //Other
+    private lateinit var navHost: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Setup nav host
+        navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+
+    }
+
+
+    override fun onNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onNavigateUp()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/register/RegisterFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import coil.load
 import com.jmzd.ghazal.recipeappmvvm.R
+import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentRegisterBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 class RegisterFragment : Fragment() {
+
+    //Binding
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init views
+        binding.apply {
+            coverImg.load(R.drawable.register_logo)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
