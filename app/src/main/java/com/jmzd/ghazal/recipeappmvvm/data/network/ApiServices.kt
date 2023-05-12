3c201716c7a71782521545673283801422c24b06
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-05-12 18:53:22 +0430

    added network module (retrofit)

---
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/network/ApiServices.kt
@@ -0,0 +1,22 @@
+package com.jmzd.ghazal.recipeappmvvm.data.network
+
+
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiServices {
+//    @POST("users/connect")
+//    suspend fun postRegister(@Query(API_KEY) apiKey: String, @Body body: BodyRegister): Response<ResponseRegister>
+//
+//    @GET("recipes/complexSearch")
+//    suspend fun getRecipes(@QueryMap queries: Map<String, String>): Response<ResponseRecipes>
+//
+//    @GET("recipes/{id}/information")
+//    suspend fun getDetail(@Path("id") id: Int, @Query(API_KEY) apiKey: String): Response<ResponseDetail>
+//
+//    @GET("recipes/{id}/similar")
+//    suspend fun getSimilarRecipes(@Path("id") id: Int, @Query(API_KEY) apiKey: String): Response<ResponseSimilar>
+//
+//    @GET("recipes/random")
+//    suspend fun getRandomRecipe(@QueryMap queries: Map<String, String>): Response<ResponseLucky>
+}
