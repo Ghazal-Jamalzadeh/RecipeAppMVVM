3680646dd2df69f3bbc8b0f67890a129ccd3b778
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-05-16 20:39:59 +0430

    call postRegisterMethod from viewModel

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/viewmodel/RegisterViewModel.kt
@@ -1,14 +1,26 @@
 package com.jmzd.ghazal.recipeappmvvm.viewmodel
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import com.jmzd.ghazal.recipeappmvvm.data.repository.RegisterRepository
+import com.jmzd.ghazal.recipeappmvvm.models.register.BodyRegister
 import com.jmzd.ghazal.recipeappmvvm.models.register.ResponseRegister
+import com.jmzd.ghazal.recipeappmvvm.utils.NetworkRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 @HiltViewModel
-class RegisterViewModel @Inject constructor(private val repository: RegisterRepository){
+class RegisterViewModel @Inject constructor(private val repository: RegisterRepository) : ViewModel(){
 
     //API
-    val registerLiveData  = MutableLiveData<ResponseRegister>()
+    val registerLiveData = MutableLiveData<NetworkRequest<ResponseRegister>>()
+    fun callRegisterApi(apiKey: String, body: BodyRegister) = viewModelScope.launch {
+        registerLiveData.value = NetworkRequest.Loading()
+        val response  : Response<ResponseRegister> = repository.postRegister(apiKey, body)
+//        registerData.value = NetworkResponse(response).generalNetworkResponse()
+    }
 }
