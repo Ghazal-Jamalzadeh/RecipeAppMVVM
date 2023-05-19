220f8ad721b3f485e264a0263a3121c1989a3c73
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-05-19 18:17:32 +0430

    completed Splash Logic (90%)

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/splash/SplashFragment.kt
@@ -1,14 +1,23 @@
 package com.jmzd.ghazal.recipeappmvvm.ui.splash
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import coil.load
+import com.jmzd.ghazal.recipeappmvvm.BuildConfig
 import com.jmzd.ghazal.recipeappmvvm.R
-import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentRegisterBinding
 import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentSplashBinding
+import com.jmzd.ghazal.recipeappmvvm.models.register.RegisterStoredModel
+import com.jmzd.ghazal.recipeappmvvm.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
@@ -16,6 +25,8 @@ class SplashFragment : Fragment() {
     //Binding
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: RegisterViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +36,31 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding.apply {
+//Dynamically background
+            bgImg.load(R.drawable.bg_splash)
+            //Application Version
+            versionTxt.text = "${getString(R.string.version)} : ${BuildConfig.VERSION_NAME}"
+            //Auto navigate
+            lifecycleScope.launchWhenCreated {
+                delay(2500)
+                //Check user info
+                viewModel.readData.asLiveData().observe(viewLifecycleOwner) { storedData : RegisterStoredModel->
+                    findNavController().popBackStack(R.id.splashFragment, true)
 
+                    if (storedData.username.isNotEmpty()) {
+                        //navigate to home
+                    } else {
+                        /* کدهای زیر در واقع مشابه هستن */
+//                        findNavController().navigate(R.id.registerFragment)
+                        findNavController().navigate(R.id.action_to_registerFragment)
+                    }
+                }
+            }
         }
     }
 
