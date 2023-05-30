c0731584b3f8452acfe777fe42dae8cc8bc2cec5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-05-30 20:35:23 +0430

    added RecipeRepository

---
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/repository/RecipeRepository.kt
@@ -0,0 +1,10 @@
+package com.jmzd.ghazal.recipeappmvvm.data.repository
+
+import com.jmzd.ghazal.recipeappmvvm.data.source.RemoteDataSource
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+class RecipeRepository @Inject constructor(remoteDataSource: RemoteDataSource) {
+    val remote = remoteDataSource
+}
