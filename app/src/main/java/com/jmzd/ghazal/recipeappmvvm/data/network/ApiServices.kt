8d84c60a6e7b3c396dc7c92cf1331bfd16b3e63a
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-05-30 20:13:26 +0430

    added getRecipes to ApiServices

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/network/ApiServices.kt
@@ -1,6 +1,7 @@
 package com.jmzd.ghazal.recipeappmvvm.data.network
 
 
+import com.jmzd.ghazal.recipeappmvvm.models.recipe.ResponseRecipes
 import com.jmzd.ghazal.recipeappmvvm.models.register.BodyRegister
 import com.jmzd.ghazal.recipeappmvvm.models.register.ResponseRegister
 import com.jmzd.ghazal.recipeappmvvm.utils.Constants.API_KEY
@@ -11,8 +12,8 @@ interface ApiServices {
     @POST("users/connect")
     suspend fun postRegister(@Query(API_KEY) apiKey: String, @Body body: BodyRegister): Response<ResponseRegister>
 
-//    @GET("recipes/complexSearch")
-//    suspend fun getRecipes(@QueryMap queries: Map<String, String>): Response<ResponseRecipes>
+    @GET("recipes/complexSearch")
+    suspend fun getRecipes(@QueryMap queries: Map<String, String>): Response<ResponseRecipes>
 //
 //    @GET("recipes/{id}/information")
 //    suspend fun getDetail(@Path("id") id: Int, @Query(API_KEY) apiKey: String): Response<ResponseDetail>
