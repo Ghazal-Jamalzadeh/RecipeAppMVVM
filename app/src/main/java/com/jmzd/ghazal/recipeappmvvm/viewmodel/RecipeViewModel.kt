c988709a9f04a3149840c730347ac5b548dc9e3f
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-02 20:02:16 +0430

    RecipeViewModel.kt -> call recent api and stuff

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/viewmodel/RecipeViewModel.kt
@@ -34,4 +34,39 @@ class RecipeViewModel @Inject constructor(private val repository: RecipeReposito
         val response : Response<ResponseRecipes> = repository.remote.getRecipes(queries)
         popularLiveData.value = NetworkResponse(response).generalNetworkResponse()
     }
+
+
+    //---Recent---//
+    //Queries
+    fun recentQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries[Constants.API_KEY] = Constants.MY_API_KEY
+        queries[Constants.TYPE] = Constants.MAIN_COURSE
+        queries[Constants.DIET] = Constants.GLUTEN_FREE
+        queries[Constants.NUMBER] = Constants.FULL_COUNT.toString()
+        queries[Constants.ADD_RECIPE_INFORMATION] = Constants.TRUE
+        return queries
+    }
+
+    //Api
+    val recentsLiveData = MutableLiveData<NetworkRequest<ResponseRecipes>>()
+    fun getRecents(queries: Map<String, String>) = viewModelScope.launch {
+        recentsLiveData.value = NetworkRequest.Loading()
+        val response = repository.remote.getRecipes(queries)
+        recentsLiveData.value = recentNetworkResponse(response)
+    }
+
+
+    private fun recentNetworkResponse(response: Response<ResponseRecipes>): NetworkRequest<ResponseRecipes> {
+        return when {
+            response.message().contains("timeout") -> NetworkRequest.Error("Timeout")
+            response.code() == 401 -> NetworkRequest.Error("You are not authorized")
+            response.code() == 402 -> NetworkRequest.Error("Your free plan finished")
+            response.code() == 422 -> NetworkRequest.Error("Api key not found!")
+            response.code() == 500 -> NetworkRequest.Error("Try again")
+            response.body()!!.results.isNullOrEmpty() -> NetworkRequest.Error("Not found any recipe!")
+            response.isSuccessful -> NetworkRequest.Success(response.body()!!)
+            else -> NetworkRequest.Error(response.message())
+        }
+    }
 }
