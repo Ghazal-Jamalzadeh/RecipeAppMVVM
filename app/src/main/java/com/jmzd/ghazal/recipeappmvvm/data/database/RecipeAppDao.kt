04c5b877192791a16e5b7d90b52126e5d13d0482
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-04 19:36:36 +0430

    cash data -> added recipe dao

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDao.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDao.kt
@@ -1,4 +1,18 @@
 package com.jmzd.ghazal.recipeappmvvm.data.database
 
-class RecipeAppDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.jmzd.ghazal.recipeappmvvm.utils.Constants
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipeAppDao {
+    //Recipe
+    @Insert(onConflict = REPLACE)
+    suspend fun saveRecipes(entity: RecipeEntity)
+
+    @Query("SELECT * FROM ${Constants.RECIPE_TABLE_NAME} ORDER BY ID ASC")
+    fun loadRecipes(): Flow<List<RecipeEntity>>
 }
