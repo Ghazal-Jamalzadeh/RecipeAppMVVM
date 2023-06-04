a436f6f1b89da515bb4dc499a5b312d91a147c5a
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-04 16:51:04 +0430

    cash data -> added RecipeEntity.kt

---
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDao.kt
@@ -0,0 +1,4 @@
+package com.jmzd.ghazal.recipeappmvvm.data.database
+
+class RecipeAppDao {
+}
---
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeEntity.kt
@@ -0,0 +1,13 @@
+package com.jmzd.ghazal.recipeappmvvm.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.jmzd.ghazal.recipeappmvvm.models.recipe.ResponseRecipes
+import com.jmzd.ghazal.recipeappmvvm.utils.Constants
+
+@Entity(tableName = Constants.RECIPE_TABLE_NAME)
+data class RecipeEntity (
+    @PrimaryKey(autoGenerate = false)
+    var id : Int = 0 ,
+    var response : ResponseRecipes
+        )
