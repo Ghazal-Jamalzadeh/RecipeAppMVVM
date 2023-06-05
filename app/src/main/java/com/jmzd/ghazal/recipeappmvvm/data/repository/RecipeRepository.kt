0b4647e3084acdba50f42e6508700f16ead96755
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-05 14:11:20 +0430

    cash data -> added localDataSource to RecipeRepository.kt

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/repository/RecipeRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/repository/RecipeRepository.kt
@@ -1,10 +1,12 @@
 package com.jmzd.ghazal.recipeappmvvm.data.repository
 
+import com.jmzd.ghazal.recipeappmvvm.data.source.LocalDataSource
 import com.jmzd.ghazal.recipeappmvvm.data.source.RemoteDataSource
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
 @ActivityRetainedScoped
-class RecipeRepository @Inject constructor(remoteDataSource: RemoteDataSource) {
+class RecipeRepository @Inject constructor(remoteDataSource: RemoteDataSource , localDataSource: LocalDataSource) {
     val remote = remoteDataSource
+    val local = localDataSource
 }
