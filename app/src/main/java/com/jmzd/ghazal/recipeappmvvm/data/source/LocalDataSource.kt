e68b5cf6ff22251b67111870ff59065955a78aa5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-05 14:05:41 +0430

    cash data -> added LocalDataSource.kt

---
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/source/LocalDataSource.kt
@@ -0,0 +1,15 @@
+package com.jmzd.ghazal.recipeappmvvm.data.source
+
+import com.jmzd.ghazal.recipeappmvvm.data.database.RecipeAppDao
+import com.jmzd.ghazal.recipeappmvvm.data.database.RecipeEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(private val dao: RecipeAppDao) {
+
+    //Recipes
+    suspend fun saveRecipes(entity: RecipeEntity) : Unit = dao.saveRecipes(entity)
+    fun loadRecipes() : Flow<List<RecipeEntity>> = dao.loadRecipes()
+
+
+}
