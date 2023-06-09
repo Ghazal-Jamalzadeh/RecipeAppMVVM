2213f1950f9bc30c6254df7cdd8f52169e111681
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-09 13:53:05 +0430

    menu -> access viewModel in MenuFragment.kt

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/menu/MenuFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.jmzd.ghazal.recipeappmvvm.R
 import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentMenuBinding
+import com.jmzd.ghazal.recipeappmvvm.viewmodel.MenuViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +17,14 @@ class MenuFragment : Fragment() {
     //Binding
     private var _binding: FragmentMenuBinding? = null
     private val binding get() = _binding!!
+
+    //ViewModel
+    private lateinit var viewModel: MenuViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[MenuViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
