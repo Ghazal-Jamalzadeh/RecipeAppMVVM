6d457cae86d7068d7209708cc4f866537771f99b
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-21 20:09:55 +0430

    DetailViewModel.kt -> call Detail api

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/viewmodel/DetailViewModel.kt
@@ -1,12 +1,26 @@
 package com.jmzd.ghazal.recipeappmvvm.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jmzd.ghazal.recipeappmvvm.data.repository.RecipeRepository
+import com.jmzd.ghazal.recipeappmvvm.models.detail.ResponseDetail
+import com.jmzd.ghazal.recipeappmvvm.utils.NetworkRequest
+import com.jmzd.ghazal.recipeappmvvm.utils.NetworkResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(private val repository: RecipeRepository) : ViewModel() {
+    //Api
+    val detailLiveData = MutableLiveData<NetworkRequest<ResponseDetail>>()
 
+    fun callDetailApi(id: Int, apiKey: String) = viewModelScope.launch {
+        detailLiveData.value = NetworkRequest.Loading()
+        val response : Response<ResponseDetail> = repository.remote.getDetail(id, apiKey)
+        detailLiveData.value = NetworkResponse(response).generalNetworkResponse()
+    }
 }
