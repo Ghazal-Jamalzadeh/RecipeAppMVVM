ba9fb7456e94dbe3fd44ee464ab89d19d81be99a
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-29 17:31:17 +0430

    cache detail -> local datasource

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/source/LocalDataSource.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/source/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.jmzd.ghazal.recipeappmvvm.data.source
 
 import com.jmzd.ghazal.recipeappmvvm.data.database.RecipeAppDao
+import com.jmzd.ghazal.recipeappmvvm.data.database.entity.DetailEntity
 import com.jmzd.ghazal.recipeappmvvm.data.database.entity.RecipeEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -11,5 +12,9 @@ class LocalDataSource @Inject constructor(private val dao: RecipeAppDao) {
     suspend fun saveRecipes(entity: RecipeEntity) : Unit = dao.saveRecipes(entity)
     fun loadRecipes() : Flow<List<RecipeEntity>> = dao.loadRecipes()
 
+    //Detail
+    suspend fun saveDetail(entity: DetailEntity) = dao.saveDetail(entity)
+    fun loadDetail(id: Int) = dao.loadDetailById(id)
+    fun existsDetail(id: Int) = dao.existsDetail(id)
 
 }
