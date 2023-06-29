8d7d4609fcd3b7ce2f6786e54dda9611b8734774
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-29 17:33:33 +0430

    cache detail -> added detail entity to database and increase database version

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDatabase.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.jmzd.ghazal.recipeappmvvm.data.database.entity.RecipeEntity
 
-@Database(entities = [RecipeEntity::class] , version = 1 , exportSchema = false)
+@Database(entities = [RecipeEntity::class , RecipeEntity:: class ] , version = 2 , exportSchema = false)
 @TypeConverters(RecipeAppTypeConverter::class)
 abstract class RecipeAppDatabase : RoomDatabase() {
     abstract fun dao() : RecipeAppDao
