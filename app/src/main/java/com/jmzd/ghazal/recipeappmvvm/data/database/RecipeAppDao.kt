96f79310f9fe7dcfa55a0bf14f97a16c59af1486
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-29 17:24:56 +0430

    cache detail -> dao

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDao.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
+import com.jmzd.ghazal.recipeappmvvm.data.database.entity.DetailEntity
 import com.jmzd.ghazal.recipeappmvvm.data.database.entity.RecipeEntity
 import com.jmzd.ghazal.recipeappmvvm.utils.Constants
 import kotlinx.coroutines.flow.Flow
@@ -16,4 +17,16 @@ interface RecipeAppDao {
 
     @Query("SELECT * FROM ${Constants.RECIPE_TABLE_NAME} ORDER BY ID ASC")
     fun loadRecipes(): Flow<List<RecipeEntity>>
+
+    //Detail
+    @Insert(onConflict = REPLACE)
+    suspend fun saveDetail(entity: DetailEntity)
+
+    @Query("SELECT * FROM ${Constants.DETAIL_TABLE_NAME} WHERE id = :id")
+    fun loadDetailById(id: Int): Flow<DetailEntity>
+
+    @Query("SELECT EXISTS (SELECT 1 FROM ${Constants.DETAIL_TABLE_NAME} WHERE ID = :id)")
+    fun existsDetail(id: Int): Flow<Boolean>
+
+
 }
