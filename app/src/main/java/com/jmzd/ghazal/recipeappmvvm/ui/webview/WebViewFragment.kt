253c84e5abb3bd98976615cf28e3f26c2a8bb9b4
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-29 13:00:02 +0430

    completed WebViewFragment.kt

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/webview/WebViewFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/webview/WebViewFragment.kt
@@ -6,6 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.jmzd.ghazal.recipeappmvvm.R
 import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentSplashBinding
 import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentWebViewBinding
@@ -18,21 +24,62 @@ class WebViewFragment : Fragment() {
     private var _binding: FragmentWebViewBinding? = null
     private val binding get() = _binding!!
 
+    //Args
+    private val args: WebViewFragmentArgs by navArgs()
+
+    //other
+    private lateinit var link: String
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentWebViewBinding.inflate(inflater , container , false)
+        _binding = FragmentWebViewBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //Args
+        args.let {
+            link = it.url
+        }
         //InitViews
         binding.apply {
+            //Back
+            backImg.setOnClickListener { findNavController().popBackStack() }
+            //Web view
+            link.let { link ->
+                webView.apply {
+                    settings.apply {
+                        javaScriptEnabled = true
+                        loadWithOverviewMode = true
+                        useWideViewPort = true
+                        builtInZoomControls = false
+                        domStorageEnabled = true
+                        databaseEnabled = true
+                    }
+                    webViewClient = WebViewClient()
+                    isVerticalScrollBarEnabled = true
+                    isHorizontalScrollBarEnabled = true
+                    webChromeClient = object : WebChromeClient() {
+                        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                            if (newProgress < 100 && webViewLoading.visibility == View.GONE) {
+                                webViewLoading.isVisible = true
+                            }
+                            webViewLoading.progress = newProgress
+                            if (newProgress == 100) {
+                                webViewLoading.isVisible = false
+                            }
+                        }
+                    }
+                    loadUrl(link)
+                }
+            }
 
-        }}
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
