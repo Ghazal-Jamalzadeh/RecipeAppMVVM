2f07ac1ca2b4176b840b06294eaf50739e28e358
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-29 17:15:30 +0430

    cache detail -> added type converter

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppTypeConverter.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/data/database/RecipeAppTypeConverter.kt
@@ -2,6 +2,7 @@ package com.jmzd.ghazal.recipeappmvvm.data.database
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
+import com.jmzd.ghazal.recipeappmvvm.models.detail.ResponseDetail
 import com.jmzd.ghazal.recipeappmvvm.models.recipe.ResponseRecipes
 
 class RecipeAppTypeConverter {
@@ -16,4 +17,16 @@ class RecipeAppTypeConverter {
     fun stringToRecipe(data: String): ResponseRecipes {
         return gson.fromJson(data, ResponseRecipes::class.java)
     }
+
+    @TypeConverter
+    fun detailToJson(recipe: ResponseDetail): String {
+        return gson.toJson(recipe)
+    }
+
+    @TypeConverter
+    fun stringToDetail(data: String): ResponseDetail {
+        return gson.fromJson(data, ResponseDetail::class.java)
+    }
+
+
 }
