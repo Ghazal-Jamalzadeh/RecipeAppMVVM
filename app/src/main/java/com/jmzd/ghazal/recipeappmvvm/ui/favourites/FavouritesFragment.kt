ae1453e2ef81af11c924c1d691b2c5fe24be523c
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-06-30 18:43:59 +0430

    favorites fragment -> completed

--- app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/recipeappmvvm/ui/favourites/FavouritesFragment.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jmzd.ghazal.recipeappmvvm.R
+import com.jmzd.ghazal.recipeappmvvm.adapter.FavoriteAdapter
 import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentDetailBinding
 import com.jmzd.ghazal.recipeappmvvm.databinding.FragmentFavouritesBinding
+import com.jmzd.ghazal.recipeappmvvm.ui.recipe.RecipeFragmentDirections
+import com.jmzd.ghazal.recipeappmvvm.utils.isVisible
+import com.jmzd.ghazal.recipeappmvvm.utils.setupRecyclerview
 import com.jmzd.ghazal.recipeappmvvm.viewmodel.FavoriteViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -20,8 +26,12 @@ class FavouritesFragment : Fragment() {
     private var _binding: FragmentFavouritesBinding? = null
     private val binding get() = _binding!!
 
-    //Other
+    //ViewModel
     private val viewModel: FavoriteViewModel by viewModels()
+
+    //Adapter
+    @Inject
+    lateinit var favoriteAdapter: FavoriteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,10 +41,27 @@ class FavouritesFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //InitViews
         binding.apply {
-
+            //Load favorites
+            viewModel.favoritesLiveData.observe(viewLifecycleOwner) {
+                if (it.isNotEmpty()) {
+                    emptyTxt.isVisible(false, favoriteList)
+                    //Data
+                    favoriteAdapter.setData(it)
+                    favoriteList.setupRecyclerview(LinearLayoutManager(requireContext()), favoriteAdapter)
+                    //Click
+                    favoriteAdapter.setOnItemClickListener { id ->
+                        val action = RecipeFragmentDirections.actionToDetailFragment(id)
+                        findNavController().navigate(action)
+                    }
+                } else {
+                    emptyTxt.isVisible(true, favoriteList)
+                }
+            }
         }
     }
 
